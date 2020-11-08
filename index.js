6806c18eb17f02a46de74ec4b2630da51b1ad0a8
Emily <e.charlotte94@gmail.com>
2020-11-08 15:16:24 +0000

    New Start
    Started again in order to get used to setting up the api and handlebars etc

--- index.js
+++ index.js
@@ -1,32 +1,95 @@
+// IMPORT PACKAGES //
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
+const app = express();
+const FA = require('@fortawesome/fontawesome-free');
 const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/indexRouter');
-const weatherRouter = require('./routes/weatherRouter');
-const errRouter = require('./routes/errRouter');
+// IMPORT CUSTOM FUNCTIONS //
+const getWeather = require('./lib/getWeather');
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
+//BODY PARSER WITH APP.USE TO USE FOR THE WHOLE PROJECT//
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//JOIN CURRENT DIR TO PUBLIC DIR//
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('hbs', hbs({
-   defaultLayout: 'main',
+   defaultLayout:'main',
    extname: '.hbs',
-   layoutsDir: path.join(__dirname, 'views','layouts'),
-   partialsDir: path.join(__dirname, 'views','partials')
+   layoutsDir: path.join(__dirname, 'views', 'layouts'),
+   partialsDir: path.join(__dirname, 'views', 'partials'),
 }));
 
+//IMPORT SETTERS//
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
-app.use('/', indexRouter);
-app.use('/weather', weatherRouter);
-app.use('*', errRouter);
+//ASYNC GET TO LOAD WEATHER INFO FOR THE INDEX PAGE//
+app.get('/', async(req, res) => {
+   let data = await getWeather("Manchester", "uk");
+   let name = data.name;
+   let temp = Math.round(data.main.temp);
+   let date = new Date();
+   let icon = data.weather[0].icon;
+   let country = data.sys.country;
+   let description = data.weather[0].description;
+   let time = new Date();
+   res.render('index', {
+      name,
+      temp,
+      country,
+      description,
+      icon,
+      date: date.toDateString(),
+      time: time.toTimeString()
+   })
+});
 
+//ADD GETTERS FOR ALL PAGES//
+app.get('/weather', (req, res) => {
+   res.render('weather');
+});
+app.get('*', (req, res) => {
+   res.render('404');
+});
+
+//ADD POST FUNCTION FOR /WEATHER //
+app.post('/', async(req, res) => {
+   let location = req.body.location;
+   let countryCode = req.body.countryCode;
+   let data = await getWeather(location, countryCode);
+   if(data.cod == '404') {
+      res.render('index', {
+         err:'The location provided does not exist'
+      });
+      return;
+   };
+   let name = data.name;
+   let temp = Math.round(data.main.temp);
+   let date = new Date();
+   let icon = data.weather[0].icon;
+   let country = data.sys.country;
+   let description = data.weather[0].description;
+   let time = new Date();
+   res.render('index', {
+      name,
+      temp,
+      country,
+      description,
+      icon,
+      date: date.toDateString(),
+      time: time.toTimeString(),
+      listExists: true
+   });
+
+
+   console.log(req.body);
+   res.render('weather');
+})
+
+//ADD LISTENER FOR THE PORT REQUEST//
 app.listen(3000, () => {
-   console.log('listening on port 3000');
+   console.log("Listening on port 3000");
 });
